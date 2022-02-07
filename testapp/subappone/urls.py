89aae9e67ac14ab772e91addd780b2a953d1796f
savantakash <88370309+savantakash@users.noreply.github.com>
2022-02-07 06:43:56 +0530

    Updated Twitter

--- testapp/subappone/urls.py
+++
@@ -1,8 +0,0 @@
-from unicodedata import name
-from django import views
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about')
-]
